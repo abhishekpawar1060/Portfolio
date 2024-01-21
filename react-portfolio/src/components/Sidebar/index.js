25b668fa5cbd111990d170ea091e1ea6dd2d6244
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-01-21 14:54:09 +0530

    commit

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,6 @@
 import './index.scss'
 import {Link, NavLink} from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/logo-a.webp'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faEnvelope, faHome, faUser} from "@fortawesome/free-solid-svg-icons"
 import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons"
@@ -9,7 +8,6 @@ const Sidebar = () => (
     <div className="nav-bar">
         <Link className="logo" to="/">
             <img src={LogoS} alt="logo" />
-            <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
         </Link>
 
         <nav>
@@ -38,7 +36,9 @@ const Sidebar = () => (
                 <a target="_blank" rel="noreferrer" href="https://github.com/abhishekpawar1060">
                     <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
                 </a>
-            </li> 
+            </li>
+            
+             
 
         </ul>
     </div>
